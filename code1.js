11f12ee02596239a48b87c5335b3300e119c1666
NawairaSajjad <nawairasajjad97@gmail.com>
2024-02-17 21:16:37 +0500

    first commit

---
+++ code1.js
@@ -0,0 +1,6 @@
+console.log("hello world!!");
+console.log("My first program");
+console.log("Governor's Initiative ");
+console.log("why aint running in the first time ?");
+var message = "hello guys";
+console.log(message);
---
+++ code1.ts
@@ -0,0 +1,5 @@
+console.log("hello world!!");
+console.log("My first program");
+console.log("Governor's Initiative ");
+let message = "hello guys";
+console.log(message);
